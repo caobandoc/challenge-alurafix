b168a2e40aeaa08a14a596fcadd4b2ebfac414b2
caobandoc <obandocarlosandres@gmail.com>
2023-09-07 21:21:21 -0500

    footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/header/Header";
 import Banner from "./components/banner/Banner";
+import Footer from "./components/footer/Footer";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <Banner />
+    <Footer />
   </React.StrictMode>
 );
